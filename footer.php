cd6350a81e487091f6cd149f050041d10b911e9b
Marie <Marue59@hotmail.fr>
2022-08-23 16:43:57 +0200

    construction du front via la dataTable :tada:

--- footer.php
+++ footer.php
@@ -3,5 +3,17 @@
     <span class="text-muted">Copyrights 2022</span>
   </div>
 </footer>   
+    <script type="text/javascript">
+      $(document).ready( function () {
+      $('#datatable').DataTable({
+      "oLanguage": {
+     "sLengthMenu": "Afficher MENU Enregistrements",
+     "sSearch": "Rechercher:",
+     "sInfo":"Total de TOTAL enregistrements (_END_ / _TOTAL_)",
+     "oPaginate": {
+     "sNext": "Suivant",
+     "sPrevious":"Précédent"}}})
+    });
+    </script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,13 +6,17 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
+    <meta name="theme-color" content="#7952b3">
 
-    <!-- Bootstrap core CSS js -->
+    <!-- Bootstrap core CSS js jquery -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- Datatable -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+  
 
-<meta name="theme-color" content="#7952b3">
-    
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
   </head>
@@ -23,14 +27,14 @@
   <!-- Fixed navbar -->
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Application commandes</a>
+      <a class="navbar-brand" href="index.php">Application commandes</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link active" href="index.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="articles.php">Articles</a>
